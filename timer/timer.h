e802a07d2b7e89feff65b9314538eaf9209dc7ff
arcan3ry <ganlupeng@foxmail.com>
2021-06-06 21:10:23 +0800

    update

--- timer/timer.h
+++ timer/timer.h
@@ -43,13 +43,9 @@ public:
     heap_timer(int delay = 0){
         expire = time(nullptr) + delay;
     }
-    //int get_index() const { return m_index; }
-public:
     time_t expire;
-    std::function<void(int, client_data*)> cb_func1;
-    //void (* cb_func)(int, client_data*);
+    std::function<void(int, client_data*)> cb_func;
     client_data* user_data;
-//private:
     int m_index;
 };
 
@@ -64,7 +60,6 @@ public:
     time_heap():capacity(0), cur_size(0){
         array.reserve(capacity);
     }
-    //time_heap(std::vector<heap_timer*>& init_array, int cap, int size);
     ~time_heap(){
         array.clear();
     }
@@ -143,8 +138,7 @@ void time_heap::add_timer(heap_timer* timer){
 }
 
 void time_heap::del_timer(heap_timer* timer){
-    timer->cb_func1 = NULL;
-    //timer->cb_func = NULL;
+    timer->cb_func = NULL;
 }
 
 heap_timer* time_heap::top() const{
@@ -175,9 +169,8 @@ void time_heap::tick(){
         if(tmp->expire > cur){
             break;
         }
-        if(array[0]->cb_func1){
-            //array[0]->cb_func(m_epollfd, array[0]->user_data);
-            array[0]->cb_func1(m_epollfd, array[0]->user_data);
+        if(array[0]->cb_func){
+            array[0]->cb_func(m_epollfd, array[0]->user_data);
         }
         pop_timer();
         tmp = array[0];
